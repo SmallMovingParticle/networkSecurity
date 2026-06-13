1943bc792304bee8a70ea8c07131c75c19468635
SmallMovingParticle <chaudharydivyansh04@gmail.com>
2026-06-14 01:40:52 +0530

    data ingestion is woking fine

---
+++ NetworkSecurity/constants/training_pipeline/__init__.py
@@ -0,0 +1,30 @@
+## to define all the info with regard to data
+
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+
+"""
+defining common constants variable for the training pipeline
+"""
+
+TARGET_COLUMN= "Result"
+PIPELINE_NAME: str= "NetworkSecurity"
+ARTIFACT_DIR: str= "Artifacts"
+FILE_NAME:str= "PhisingData.csv"
+
+TRAIN_FILE_NAME:str= "train.csv"
+TEST_FILE_NAME:str = "test.csv"
+
+"""
+data ingestion related constants start with DATA_INGESTION variable name
+"""
+
+DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
+DATA_INGESTION_DATABASE_NAME: str = "devyansh"
+DATA_INGESTION_DIR_NAME: str="data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR:str ="feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
