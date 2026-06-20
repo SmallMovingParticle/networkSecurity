dfc5a91b08f817e5fd6768185e1e6d2c1627f437
SmallMovingParticle <chaudharydivyansh04@gmail.com>
2026-06-21 04:29:56 +0530

    pipeline done aws code written will push to aws if i get my subscription

--- NetworkSecurity/components/model_trainer.py
+++ NetworkSecurity/components/model_trainer.py
@@ -27,10 +27,10 @@ from sklearn.ensemble import (
 
 import mlflow
 from urllib.parse import urlparse
-"""
-import dagshub
-#dagshub.init(repo_owner='krishnaik06', repo_name='networksecurity', mlflow=True)
 
+import dagshub
+dagshub.init(repo_owner='chaudharydivyansh04', repo_name='networkSecurity', mlflow=True)
+"""
 os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
 os.environ["MLFLOW_TRACKING_USERNAME"]="krishnaik06"
 os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ece21e0e2adb4e36a250ae3251f"
@@ -51,7 +51,7 @@ class ModelTrainer:
     def track_mlflow(self,best_model,classificationmetric):
         ##mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
         ##tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-        mlflow.set_tracking_uri("file:///E:/NetworkSecurityProject/mlruns")
+        
         mlflow.set_experiment("NetworkSecurity")
         with mlflow.start_run():
             f1_score=classificationmetric.f1_score
@@ -145,6 +145,8 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=Network_Model)
+
+        
         #model pusher
         save_object("final_model/model.pkl",best_model)
         
